aaa25f5f58b97c3e1ece22c98f6f8bd4225af61c
nishithakur04 <129810284+nishithakur04@users.noreply.github.com>
2023-09-02 06:04:04 +0530

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <div>
+      <h2>Mountain</h2>
+      <img
+        src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.natgeofe.com%2Fn%2Fc9107b46-78b1-4394-988d-53927646c72b%2F1095_3x2.jpg&tbnid=GKstLXP_h2zAFM&vet=12ahUKEwjD8cfwv4mBAxUem2MGHTgfBlsQMygBegQIARB2..i&imgrefurl=https%3A%2F%2Fwww.nationalgeographic.com%2Fscience%2Farticle%2Fmountains&docid=xHphW-5NKedjiM&w=3072&h=2048&q=mountain&ved=2ahUKEwjD8cfwv4mBAxUem2MGHTgfBlsQMygBegQIARB2"
+        alt="Mountain"
+      />
+      <p>tel +787900000909</p>
+      <p> Email mountain@.com</p>
+    </div>
+  );
+}
+export default Card;
